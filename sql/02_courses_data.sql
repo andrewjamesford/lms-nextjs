ce248909009e613525a72988c0113dedf09ff0a5
Andrew Ford <andrew@andrewford.co.nz>
2023-09-28 08:34:23 +0000

    Added SQL for courses table

---
+++ sql/02_courses_data.sql
@@ -0,0 +1,3 @@
+insert into "courses" ("created_at", "description", "id", "image", "name") values ('2023-09-28 07:42:52.54519+00', 'This is a course for those beginning their journey with ReactJS', '1', NULL, 'ReactJS for Beginners');
+insert into "courses" ("created_at", "description", "id", "image", "name") values ('2023-09-28 07:44:28.756364+00', 'This is a course for learning all the latest in HTML', '3', NULL, 'HTML whats new');
+insert into "courses" ("created_at", "description", "id", "image", "name") values ('2023-09-28 07:44:03.255918+00', 'Learn the basics of CSS', '2', NULL, 'CSS 101');
