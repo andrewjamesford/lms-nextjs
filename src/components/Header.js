02528e950bdd3c4479f7a130138d409fb87d4a1d
Andrew Ford <andrew@andrewford.co.nz>
2023-10-28 23:46:29 +0000

    Added storybook to the project Cleaned up defaults Set up Header component in Storybook

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
 
-export default function Header() {
+import styles from "./header.module.css";
+
+const Header = () => {
     return (
-        <header>
-            <h1>Header</h1>
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                <h1>LMS</h1>
+            </div>
         </header>
     );
 }
+
+export default Header;
---
+++ src/components/Header.stories.js
@@ -0,0 +1,17 @@
+import Header from './Header';
+
+export default {
+  title: 'Components/Header',
+  component: Header,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'fullscreen',
+  },
+};
+export const Default = {
+  args: {
+  },
+};
+
